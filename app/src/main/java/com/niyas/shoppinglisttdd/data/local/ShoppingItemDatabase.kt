4fe7d004a043f44b70c1e4ea2feb7661059bd5e4
Niyas <mohamed.niyas@pearldatadirect.com>
2023-08-05 19:44:51 +0530

    db test cases added

---
+++ app/src/main/java/com/niyas/shoppinglisttdd/data/local/ShoppingItemDatabase.kt
@@ -0,0 +1,9 @@
+package com.niyas.shoppinglisttdd.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingItem::class], version = 1)
+abstract class ShoppingItemDatabase : RoomDatabase() {
+    abstract fun shoppingDao(): ShoppingDao
+}
